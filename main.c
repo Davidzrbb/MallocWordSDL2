106df23485ca5d7d8b2a366b8720ed75701e388b
David <zdavid1@myges.fr>
2021-11-26 14:29:34 +0100

    start menu

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include "include\main.h"
 
-#undef main
+
 
 int main(int argc, char *args[]) {
-     TTF_Init();
+    TTF_Init();
 
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
@@ -22,17 +22,7 @@ int main(int argc, char *args[]) {
     SDL_RenderCopy(renderer, image, NULL, NULL);
     SDL_RenderPresent(renderer);
 
-    TTF_Font *font = TTF_OpenFont("arial.ttf", 25);
-    SDL_Color color = {255, 255, 255};
-    SDL_Surface* surface = TTF_RenderText_Solid(font, "Malloc Word !", color);
-    SDL_Texture *textureFont = SDL_CreateTextureFromSurface(renderer, surface);
-    int texW = 0;
-    int texH = 0;
-    SDL_QueryTexture(textureFont, NULL, NULL, &texW, &texH);
-    SDL_Rect dstrect = {110, 20, 500, 200};
-    SDL_RenderCopy(renderer, textureFont, NULL, &dstrect);
-    SDL_RenderPresent(renderer);
-
+    writtingText(renderer);
 
     statut = EXIT_SUCCESS;
 
@@ -58,6 +48,7 @@ int main(int argc, char *args[]) {
     return statut;
 }
 
+
 int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h) {
     if (0 != SDL_Init(SDL_INIT_VIDEO)) {
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
@@ -68,23 +59,6 @@ int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h) {
         return -1;
     }
     return 0;
-}
-
-SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer) {
-    SDL_Surface *tmp = NULL;
-    SDL_Texture *texture = NULL;
-    tmp = SDL_LoadBMP(path);
-    if (NULL == tmp) {
-        fprintf(stderr, "Erreur SDL_LoadBMP : %s", SDL_GetError());
-        return NULL;
-    }
-    texture = SDL_CreateTextureFromSurface(renderer, tmp);
-    SDL_FreeSurface(tmp);
-    if (NULL == texture) {
-        fprintf(stderr, "Erreur SDL_CreateTextureFromSurface : %s", SDL_GetError());
-        return NULL;
-    }
-    return texture;
 }
 
 int setWindowColor(SDL_Renderer *renderer, SDL_Color color) {
---
+++ startMenu.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include "startMenu.h"
+
+SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer) {
+    SDL_Surface *tmp = NULL;
+    SDL_Texture *texture = NULL;
+    tmp = SDL_LoadBMP(path);
+    if (NULL == tmp) {
+        fprintf(stderr, "Erreur SDL_LoadBMP : %s", SDL_GetError());
+        return NULL;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, tmp);
+    SDL_FreeSurface(tmp);
+    if (NULL == texture) {
+        fprintf(stderr, "Erreur SDL_CreateTextureFromSurface : %s", SDL_GetError());
+        return NULL;
+    }
+    return texture;
+}
+
+void writtingText(SDL_Renderer *renderer){
+    TTF_Font *font = TTF_OpenFont("arial.ttf", 25);
+    SDL_Color color = {255, 255, 255};
+    //TITLE
+    SDL_Surface *tile = TTF_RenderText_Solid(font, "Malloc Word !", color);
+    SDL_Texture *textureFont = SDL_CreateTextureFromSurface(renderer, tile);
+    int texW = 0;
+    int texH = 0;
+    SDL_QueryTexture(textureFont, NULL, NULL, &texW, &texH);
+    SDL_Rect dstrect = {110, 20, 500, 150};
+    SDL_RenderCopy(renderer, textureFont, NULL, &dstrect);
+    SDL_RenderPresent(renderer);
+
+    //Button
+    tile = TTF_RenderText_Solid(font, "Charge !", color);
+    textureFont = SDL_CreateTextureFromSurface(renderer, tile);
+    texW = 0;
+    texH = 0;
+    SDL_QueryTexture(textureFont, NULL, NULL, &texW, &texH);
+    SDL_Rect dstrectNewGame = {50, 220, 300, 70};
+    SDL_RenderCopy(renderer, textureFont, NULL, &dstrectNewGame);
+    SDL_RenderPresent(renderer);
+
+    //Button
+    tile = TTF_RenderText_Solid(font, "Nouvelle partie !", color);
+    textureFont = SDL_CreateTextureFromSurface(renderer, tile);
+    texW = 0;
+    texH = 0;
+    SDL_QueryTexture(textureFont, NULL, NULL, &texW, &texH);
+    SDL_Rect dstrectCharge = {50, 370, 350, 70};
+    SDL_RenderCopy(renderer, textureFont, NULL, &dstrectCharge);
+    SDL_RenderPresent(renderer);
+}
+
+void mouseStartMenu(){
+
+}
